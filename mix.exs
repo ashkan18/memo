d7117e37dbf7f3a462199302a635103b49a9b674
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2021-11-15 15:40:31 -0800

    Fix build

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule Memo.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["cmd --cd assets npm run deploy", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
 end
